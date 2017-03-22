676566b8d7a355de4d80948bdf05b35ff0563671
Juan Pablo <pablo@cruzf.net>
2017-03-22 17:54:56 +0100

    compile and execute

--- datatypes.h
+++ datatypes.h
@@ -1,6 +1,9 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
-enum signs {EMPTY, POSITIVE, NEGATIVE};
+
+#include <stdint.h>
+
+enum signs {EMPTY, POSITIVE=1, NEGATIVE=-1};
 
 typedef struct  {
     enum sign sign;
@@ -15,6 +18,24 @@ typedef struct {
 struct Memory {
     Register data[MAX_MEMORY];
 };
+
+typedef struct{
+    int8_t Sign;
+    union{
+        struct{
+            uint16_t COD;
+            uint32_t ADDR;
+        };
+        struct{
+            uint16_t _PAD1;
+            uint32_t DATA;
+        };
+        struct{
+            uint32_t _PAD2;
+            uint16_t INDEX;
+        };
+    };
+} memory_register;
 
 typedef struct {
     int from;
--- main.c
+++ main.c
@@ -8,78 +8,30 @@
 #include "datatypes.h"
 
 struct Memory memoria;
-Register A;
-Register X;
+void *memory;
+memory_register A;
+memory_register X;
 Index indices[6];
-Index J;
+memory_register J;
 char *CMD;
 char *line;
+memory_register *ptrAddr;
 
-
-void copyArrayMod(char *a, const char *b, Modifiers mod) {
-    int i = (mod.from)-1;
-    int l = (mod.to)-1;
-    while((i <= l) && (*a++ = *(b+i++)));
-}
-void copyArray(char *a, const char *b) {
-    while((*a++ = *b++));
-}
-
-Register getRegisterFromAddress(int addr) {
-    Register result;
-    if(addr < MAX_MEMORY && addr >= 0) {
-        result = memoria.data[addr];
-    }
-    return result;
+memory_register* getRegisterFromAddress(int addr) {
+    memory_register* R;
+    R = &memory;
+    R += addr;
+    return R;
 }
 
 
-char getSign(enum signs sign) {
-    char s = ' ';
-    switch (sign) {
-    case POSITIVE:
-        s = '+';
-        break;
-    case NEGATIVE:
-        s = '-';
-        break;
-    default:
-        break;
-    }
-    return s;
+void setRegister(memory_register *r, uint32_t value) {
+    memory_register *ptr = &memory;
+    ptr += r->ADDR;
+    ptr->DATA = value;
+    ptr->Sign = r->Sign;
 }
 
-void printRegister(Register *r) {
-    printf("0X%x: ",&(*r));
-    printf("%c",getSign(r->sign));
-    for(int i = 0; i < sizeof(r->data); i++) {
-        printf("%c",r->data[i]);
-    }
-    printf("\n");
-}
-
-void printIndex(int i) {
-    Index *in = &(indices[i]);
-    printf("0X%x: ",&in + i);
-    printf("%c",getSign(in->sign));
-    for(int i = 0; i < sizeof(in->data); i++) {
-        printf("%c",in->data[i]);
-    }
-    printf("\n");
-}
-
-void setRegister(enum signs s, const char *value, Register *r) {
-    r->sign = s;
-    copyArray(&r->data, value);
-}
-
-void setIndex(enum signs s, const char *value, int index) {
-    int in = index-48;
-    Index *i = indices + in;
-    i->sign = s;
-    copyArray(&i->data, value);
-    indices[in] = *i;
-}
 
 Modifiers getModifiers(char *cmd,char *args) {
     Modifiers mods;
@@ -98,123 +50,148 @@ Modifiers getModifiers(char *cmd,char *args) {
     return mods;
 }
 
-void loadRegister(Register *des, char *cmd,char *args) {
-    enum signs s;
+void storeRegister(memory_register *R, int addrs) {
+    memory_register *ptr = &memory;
+    ptr += addrs;
+    *ptr = *R;
+}
 
+int run() {
+    int result = 0;
+    ptrAddr = &memory;
+    ptrAddr += J.INDEX;
+    memory_register *tmp = &memory;
+    int instr = 0;
+    while(instr++ < 10) {
+        tmp = &memory;
+        switch(ptrAddr->COD) {
+        case LDA:
+            tmp += ptrAddr->ADDR;
+            A.DATA = tmp->DATA;
+            break;
+        case LDX:
+            tmp += ptrAddr->ADDR;
+            X.DATA = tmp->DATA;
+            break;
+        case STA:
+            storeRegister(&A, ptrAddr->ADDR);
+            break;
+        case STX:
+            storeRegister(&X, ptrAddr->ADDR);
+            break;
+        case ADD:
+            tmp += ptrAddr->ADDR;
+            int v = (A.DATA * A.Sign) + (tmp->DATA * tmp->Sign);
+            A.Sign = (v > 0)?POSITIVE:NEGATIVE;
+            A.DATA = abs(v);
+            break;
+        }
+        ptrAddr++;
+    }
+
+    return result;
+}
+
+void compile(char *cmd, char *args) {
     Modifiers mods = getModifiers(cmd,args);
-    Register d = getRegisterFromAddress(mods.address);
 
-    if(mods.from == 0) {
-        mods.from = 1;
-        mods.to = 6;
-        s = d.sign;
-    }
-    char *reg2 = "";
-    copyArrayMod(&reg2,&d.data,mods);
-    setRegister(s, &reg2,des);
-}
-
-void storeRegister(Register *src, char *cmd, char *args) {
-    Modifiers mods = getModifiers(cmd, args);
-
-    if(mods.address < MAX_MEMORY && mods.address >= 0) {
-        memoria.data[mods.address] = *src;
-    }
-}
-
-void loadIndex(int i, char *cmd,char *args) {
-    enum signs s;
-
-    Modifiers mods = getModifiers(cmd,args);
-    Register d = getRegisterFromAddress(mods.address);
-
-    if(mods.from == 0) {
-        mods.from = 4;
-        mods.to = 6;
-        s = d.sign;
-    }
-    char *reg2 = "";
-    copyArrayMod(&reg2,&d.data,mods);
-    setIndex(s, &reg2, i);
-}
-
-void add(char *cmd, char *args) {
-    Modifiers mods = getModifiers(cmd,args);
-    Register d = getRegisterFromAddress(mods.address);
-
-    int rA = atoi(A.data);
-    int rR = atoi(d.data);
-
-    switch (A.sign) {
-    case NEGATIVE:
-        rA *= -1;
-        break;
-    }
-    switch (d.sign) {
-    case NEGATIVE:
-        rR *= -1;
-        break;
-    }
-
-    rA += rR;
-    if(rA >= 0) {
-        A.sign = POSITIVE;
-    } else {
-        A.sign = NEGATIVE;
-    }
-    sprintf(A.data, "%d", abs(rA));
-}
-
-void process(char *cmd, char *args) {
     if(strcmp(cmd,"LDA") == 0) {
-        loadRegister(&A,cmd, args);
+        ptrAddr->COD = LDA;
+        ptrAddr->ADDR = mods.address;
+        ptrAddr++;
     } else if(strcmp(cmd,"LDX") == 0) {
-        loadRegister(&X,cmd, args);
+        ptrAddr->COD = LDX;
+        ptrAddr->ADDR = mods.address;
+        ptrAddr++;
     } else if((strlen(cmd) == 3) && cmd[0] == 'L' && cmd[1] == 'D') {
-        loadIndex(cmd[2]-1,cmd, args);
+        switch(cmd[2]) {
+        case '1':
+            ptrAddr->COD = LD1;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        case '2':
+            ptrAddr->COD = LD2;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        case '3':
+            ptrAddr->COD = LD3;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        case '4':
+            ptrAddr->COD = LD4;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        case '5':
+            ptrAddr->COD = LD5;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        case '6':
+            ptrAddr->COD = LD6;
+            ptrAddr->ADDR = mods.address;
+            ptrAddr++;
+            break;
+        }
     } else if(strcmp(cmd,"STA") == 0) {
-        storeRegister(&A, cmd, args);
+        ptrAddr->COD = STA;
+        ptrAddr->ADDR = mods.address;
+        ptrAddr++;
     } else if(strcmp(cmd,"STX") == 0) {
-       storeRegister(&X, cmd, args);
+        ptrAddr->COD = STX;
+        ptrAddr->ADDR = mods.address;
+        ptrAddr++;
     } else if(strcmp(cmd,"ADD") == 0) {
-       add(cmd, args);
+       ptrAddr->COD = ADD;
+       ptrAddr->ADDR = mods.address;
+       ptrAddr++;
+    } else if(strcmp(cmd,"ORIG") == 0) {
+       Modifiers mods = getModifiers(cmd,args);
+       J.INDEX = mods.address;
+       ptrAddr += J.INDEX;
     }
 }
 
-void printMemory() {
-    int j = 0;
-    for(int i = 0; i < (sizeof(memoria.data)/sizeof(Register)); i++) {
-        //printRegister(memoria.data+i);
-        printf("0x%x: ",&memoria.data[i]);
-        printf("%c",getSign(memoria.data[i].sign));
-        for(int k = 0; k < sizeof(memoria.data[i].data); k++) {
-            printf("%c",memoria.data[i].data[k]);
+void printInnerMemory() {
+    int j = 1;
+    memory_register *it = &memory;
+    memory_register *value;
+    printf("Sizeof: %u\n",sizeof(memory_register));
+    while(it != ptrAddr) {
+        value = &memory;
+        value += it->ADDR;
+        printf("%u %u:%c%u",((((int)&(*it)) - (int)&memory))/sizeof(memory_register),it->COD,(it->Sign>0)?'+':'-',it->ADDR);
+        if(&(*it) > value) {
+            printf(" %u",value->COD);
         }
-        printf("  ");
-        if(j++ % 5 == 0) {
+        printf("\t");
+        //if(!(j++ % 7))
             printf("\n");
-        }
+        it++;
     }
+
     printf("\n");
 }
 
 void printSpecialRegisters() {
-    printf("Register A: ");
-    printRegister(&A);
-    printf("Register X: ");
-    printRegister(&X);
+    printf("\n\n\t Special Registers \n");
 
-    for(int i = 0; i < sizeof(indices)/sizeof(Index); i++) {
-        printf("Index%d: ",(i+1));
-        printIndex(i);
-    }
+    printf("Register A: ");
+    printf("0X%x: ",&A);
+    printf("%c%u",(A.Sign > 0)?'+':'-',A.DATA);
+    printf("\n");
+
+    printf("Register X: ");
+    printf("0X%x: ",&X);
+    printf("%c%u",(X.Sign > 0)?'+':'-',X.DATA);
+    printf("\n");
 
     printf("Index J: ");
     printf("0X%x: ",&J);
-    printf("%c",getSign(J.sign));
-    for(int i = 0; i < sizeof(J.data); i++) {
-        printf("%c",J.data[i]);
-    }
+    printf("%u",J.INDEX);
     printf("\n");
 }
 
@@ -224,31 +201,46 @@ void parseCode(const char* src) {
 
     while(line != NULL) {
         char *end_token;
-        printf("%s\n",line);
         char *cmd = strtok_s(line, " ",&end_token);
         char *args = strtok_s(NULL," ",&end_token);
-        process(cmd,args);
+        compile(cmd,args);
         line = strtok_s(NULL, "\n", &end_str);
     }
 }
 
-char *source = "LD1 2000(0:2)\n\
+char *source = "ORIG 2015\n\
+               LD1 2000(0:2)\n\
                LD6 2000(0:2)\n\
                LDX 2000\n\
                STX 2001\n\
-               LDA 3999\n\
-               ADD 2000\n";
+               LDA 2000\n\
+               ADD 1999\n\
+               STA 2002\n\
+               LDA 2016\n";
 
 int main(int argc, char *argv[])
 {
+    memory = malloc( MAX_MEMORY * sizeof(memory_register));
+    ptrAddr = &memory;
+    J.DATA = 0;
+    A.Sign = POSITIVE;
+    X.Sign = POSITIVE;
 
-    setRegister(NEGATIVE,"80354",&memoria.data[2000]);
-    setRegister(POSITIVE,"11111",&memoria.data[3999]);
+    memory_register r1;
+    r1.ADDR = 2000;
+    r1.Sign = POSITIVE;
+    memory_register r2;
+    r2.ADDR = 1999;
+    r2.Sign = NEGATIVE;
+
+    setRegister(&r1, 80354);
+    setRegister(&r2, 11111);
 
     parseCode(source);
-    printMemory();
+
+    run();
+    printInnerMemory();
     printSpecialRegisters();
 
-    printf("memory size: %d\n", sizeof(memoria));
     return 0;
 }
